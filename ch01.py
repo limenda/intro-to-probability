399f83e7f0f667bdc0e97db3073d458387531ceb
Andrey Lobov <andrey.lobov@NetCracker.com>
2021-07-21 22:28:13 +0300

    Example 1.2 (Coin Tossing)

--- ch01.py
+++ ch01.py
@@ -6,6 +6,7 @@ Created on Wed Jul 21 21:50:03 2021
 """
 
 import random
+from collections import Counter
 from typing import List
 
 ##########################################
@@ -18,4 +19,28 @@ def random_numbers(n: int) -> List[float]:
     ''' The function generates n random real numbers in the interval [0, 1]'''
     return [random.random() for _ in range(n)]
 
+print("/nEx. 1.1")
 print(random_numbers(20))
+
+##########################################
+#
+# Example 1.2 (Coin Tossing)
+#
+##########################################
+
+def coin_tosses(n: int) -> List[str]:
+    ''' The function returs number of heads for tossing a coin n times'''
+    coin = ['H', 'T']
+    tosses = [random.choice(coin) for _ in range(n)]
+    result = Counter(tosses)
+    return result['H']
+
+def tossing(n: int) -> None:
+    ''' just a helper function to print out the results of several experiments'''
+    heads = coin_tosses(n)
+    print(f'tossing {n} times. Heads: {heads}/{n} = {heads/n}')
+    
+print("\nEx. 1.2")
+tossing(20)
+tossing(1000)
+tossing(100000)
